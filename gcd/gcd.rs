fd2eb53ccfb3c0ac0155a0af5aa009f3d159a7be
Jani Anttonen <JaniAnttonen@gmail.com>
2017-02-23 17:06:53 +0200

    derpenings

--- gcd/gcd.rs
+++ gcd/gcd.rs
@@ -1,16 +1,40 @@
+use std::env;
+
 fn main() {
-  let mut x = 310;
-  let mut y = 5;
+  let args: Vec<_> = env::args().collect();
+
+  let mut x = match args[1].parse::<i32>() {
+    Ok(i) => i,
+    Err(e) => {
+      -1
+    }
+  };
+  let mut y = match args[2].parse::<i32>() {
+    Ok(i) => i,
+    Err(e) => {
+      -1
+    }
+  };
   let mut z;
 
   println!("The greatest common divisor of {} and {} is:", x, y);
 
   loop {
-    z = y % x;
+    z = if x > y {
+      x % y
+    } else {
+      y % x
+    };
+
+    if z == 1 {
+      println!("{}", 1);
+      break;
+    }
     if z == 0 {
       println!("{}", x);
       break;
     }
+
     x = y;
     y = z;
   }
